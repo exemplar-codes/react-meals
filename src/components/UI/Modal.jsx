02aab07953c9271fa6aee159eb7d3d7c2c83f29a
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-06-25 20:05:36 +0530

    Add generic Modal component and Cart

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+function Backdrop(props) {
+  return <div className={classes.backdrop} />;
+}
+
+function ModalOverlay(props) {
+  return (
+    <div className={classes.modal}>{props.children}</div>
+  );
+}
+
+const portalElement = document.getElementById("overlays");
+
+function Modal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+}
+
+export default Modal;
