39a485387cb90006b64fbd3d484eadabe52014e7
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-06-25 19:16:41 +0530

    Complete MealItem form

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import classes from "./MealItem.module.css";
 
+import MealItemForm from "./MealItemForm";
+
 function MealItem(props, defaultProps) {
   const price = `${props.price.toFixed(2)}`;
   console.log(defaultProps.key);
@@ -12,7 +14,9 @@ function MealItem(props, defaultProps) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Input from "../../UI/Input";
+
+import classes from "./MealItemForm.module.css";
 
 function MealItemForm() {
-    
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          // props for HTML input
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
 }
 
 export default MealItemForm;
