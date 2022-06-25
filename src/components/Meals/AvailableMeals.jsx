9134c8bbf906b992236c4c84439acf2f89b0c222
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-06-25 19:00:42 +0530

    Add Mealitem1

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
+import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
@@ -30,11 +33,20 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 }
---
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MealItemForm() {
+    
+}
+
+export default MealItemForm;
